9f0be7b1431797efcd1f10aff5fe54de0ad1dc43
Joey Payton <joey.payton@gmail.com>
2024-03-17 22:41:52 -0400

    Smoothed out semantic mapper some.

--- SemanticMapper.py
+++ SemanticMapper.py
@@ -4,9 +4,9 @@ from sentence_transformers import SentenceTransformer
 import faiss
 
 class SemanticMapper:
-    def __init__(self, function_ids_to_descriptions = None, similarity_threshold = 0.75, embedding_model = None):
+    def __init__(self, ids_to_descriptions = None, similarity_threshold = 0.75, embedding_model = None):
         self.words = []
-        self.functions = []
+        self.ids = []
         self.d = 0
         self.index = None
 
@@ -17,30 +17,45 @@ class SemanticMapper:
         else:
             self.model = embedding_model
 
-        if function_ids_to_descriptions is not None:
-            self.f_id_to_descriptions = function_ids_to_descriptions
+        if ids_to_descriptions is not None:
+            self.id_to_descriptions = ids_to_descriptions
             self.build_index()
         else:
-            self.f_id_to_descriptions = {}
+            self.id_to_descriptions = {}
+
+    def add_id_and_descriptions(self, new_id, description_list, force_rebuild=False):
+        self.id_to_descriptions[new_id] = description_list
+
+        if force_rebuild:
+            self.build_index()
+        else:
+            self.words += description_list
+            self.ids += [new_id] * len(description_list)
+            added_x = self.model.encode(description_list)
+            self.index.add(added_x)
+
+    def remove_id(self, id):
+        del self.id_to_descriptions[id]
+        self.build_index()
 
     def build_index(self):
-        for key in self.f_id_to_descriptions:
-            self.words += self.f_id_to_descriptions[key]
-            self.functions += [key] * len(self.f_id_to_descriptions[key])
+        for key in self.id_to_descriptions:
+            self.words += self.id_to_descriptions[key]
+            self.ids += [key] * len(self.id_to_descriptions[key])
 
         x = self.model.encode(self.words)
         self.d = x.shape[1]
         self.index = faiss.IndexFlatL2(self.d)
         self.index.add(x)
 
-    def get_functions_for_sentence(self, sentence):
+    def parse_sentence(self, sentence, verbose=False):
         # break the string into words (making sure to take care of punctuation)
-        test_words, string_literals, numbers = self.split_string(sentence)
+        test_words, string_literals, numeric_literals = self.split_string(sentence)
 
         # get the embeddings for the words
         test_embeddings = self.model.encode(test_words)
 
-        D, I = self.index.search(test_embeddings, 3)
+        D, I = self.index.search(test_embeddings, 1)
         matching_indices = [I[i] for i in range(len(test_words))]
 
         # filter out the words that are not similar enough
@@ -50,16 +65,34 @@ class SemanticMapper:
 
         # matching indices is a list of lists, where each list contains the indices of the c
         # now, for each word, find all the categories of the words that are similar to it
-        matching_functions = []
+        matching_ids = []
         for i in range(len(test_words)):
-            matching_functions.append([self.functions[j] for j in matching_indices[i]])
+            matching_ids.append([self.ids[j] for j in matching_indices[i]])
 
-        # Now, for each word in the test string,
-        for i in range(len(test_words)):
-            print(f"Word {test_words[i]} is in category {matching_functions[i]}")
-            # then show the rounded distances of the words that are similar to it
-            for j in range(len(matching_indices[i])):
-                print(f"  Word {self.words[matching_indices[i][j]]} is at distance {round(D[i][j], 3)}")
+            # if the test word at this index is _s_literal_ or _n_literal_, then replace it with the string or number
+            # and make the id _s_literal_ or _n_literal_
+            if test_words[i] == "_s_literal_":
+                matching_ids[i] = ["_s_literal_"]
+                test_words[i] = string_literals.pop(0)
+            elif test_words[i] == "_n_literal_":
+                matching_ids[i] = ["_n_literal_"]
+                test_words[i] = numeric_literals.pop(0)
+            elif test_words[i] == "_last_result_":
+                matching_ids[i] = ["_last_result_"]
+            elif len(matching_ids[i]) == 0:
+                matching_ids[i] = ["_unknown_"]
+
+        if verbose:
+            # Now, for each word in the test string,
+            for i in range(len(test_words)):
+                print(f"Word {test_words[i]} points to the id {matching_ids[i]}")
+                # then show the rounded distances of the words that are similar to it
+                for j in range(len(matching_indices[i])):
+                    print(f"  Word {self.words[matching_indices[i][j]]} is at distance {round(D[i][j], 3)}")
+
+        # flatten the list of matching ids to the first in the list
+        matching_ids = [matching_ids[i][0] for i in range(len(matching_ids))]
+        return test_words, matching_ids
 
     @staticmethod
     def split_string(string):
@@ -97,21 +130,33 @@ class SemanticMapper:
 
 
 if __name__ == "__main__":
-    f_id_to_descriptions = {}
-    f_id_to_descriptions[1] = ["put", "place", "set", "store"]
-    f_id_to_descriptions[2] = ["get", "fetch", "retrieve", "grab", "take", "bring"]
-    f_id_to_descriptions[3] = ["webpage", "page", "site", "website"]
-    f_id_to_descriptions[4] = ["text", "text data", "words", "ascii"]
-    f_id_to_descriptions[5] = ["from", "found at", "at", "located at", "at the location of"]
-    f_id_to_descriptions[6] = ["local file", "file", "document", "record"]
-    f_id_to_descriptions[7] = ["wiki", "wikipedia", "encyclopedia"]
-    f_id_to_descriptions[8] = ["in", "into", "inside", "within"]
-    f_id_to_descriptions[9] = ["search", "look for", "find", "seek", "duckduckgo", "google"]
+    id_to_descriptions = {}
+    id_to_descriptions['put'] = ["put", "place", "set", "store"]
+    id_to_descriptions['get'] = ["get", "fetch", "retrieve", "grab", "take", "bring"]
+    id_to_descriptions['page'] = ["webpage", "page", "site", "website"]
+    id_to_descriptions['text'] = ["text", "text data", "words", "ascii"]
+    id_to_descriptions['at'] = ["from", "found at", "at", "located at", "at the location of"]
+    id_to_descriptions['file'] = ["local file", "file", "document", "record"]
+    id_to_descriptions['wikipedia'] = ["wiki", "wikipedia", "encyclopedia"]
+    id_to_descriptions['in'] = ["in", "into", "inside", "within"]
+    id_to_descriptions['search'] = ["search", "look for", "find", "seek", "duckduckgo", "google"]
 
-    sm = SemanticMapper(f_id_to_descriptions)
+    # measure the amount of time it takes to get the ids for a sentence
+    import time
+    start = time.time()
+    sm = SemanticMapper(id_to_descriptions)
+    print(f"Time to build the index: {time.time() - start}")
 
+    # lets add a new function
+    start = time.time()
+    sm.add_id_and_descriptions('delete', ["zap", "shoot", "fire", "destroy", "annihilate", "obliterate", "remove"])
+    print(f"Time to add a new function: {time.time() - start}")
+
+
+    start = time.time()
     test_string = "Get the text from the webpage 'www.google.com' and store it in the file 'output.txt'"
-    sm.get_functions_for_sentence(test_string)
+    sm.parse_sentence(test_string)
+    print(f"Time to get the functions for a sentence: {time.time() - start}")
 
-    test_string = "call the function monkeydude 44 times"
-    sm.get_functions_for_sentence(test_string)
+    test_string = "call the function kill monkeydude 44 times"
+    sm.parse_sentence(test_string)
